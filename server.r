a4276d475ebb2ec8bc0e87e6056a7e07a9870140
denversong <denversong@aliyun.com>
2016-02-18 21:33:19 +0800

    Update server.r

--- server.r
+++ server.r
@@ -1,36 +1,20 @@
-# server.R
-
-library(maps)
-library(mapproj)
-counties <- readRDS("data/counties.rds")
-source("helpers.R")
-
-shinyServer(
-  function(input, output) {
-    output$map <- renderPlot({
-      data <- switch(input$var, 
-                     "Percent White" = counties$white,
-                     "Percent Black" = counties$black,
-                     "Percent Hispanic" = counties$hispanic,
-                     "Percent Asian" = counties$asian)
-      
-      color <- switch(input$var, 
-                      "Percent White" = "darkgreen",
-                      "Percent Black" = "black",
-                      "Percent Hispanic" = "darkorange",
-                      "Percent Asian" = "darkviolet")
-      
-      legend <- switch(input$var, 
-                       "Percent White" = "% White",
-                       "Percent Black" = "% Black",
-                       "Percent Hispanic" = "% Hispanic",
-                       "Percent Asian" = "% Asian")
-      
-      percent_map(var = data, 
-                  color = color, 
-                  legend.title = legend, 
-                  max = input$range[2], 
-                  min = input$range[1])
+shinyServer(function(input, output, session) {
+  # Store in a convenience variable
+  cdata <- session$clientData
+  
+  # Values from cdata returned as text
+  output$clientdataText <- renderText({
+    cnames <- names(cdata)
+    
+    allvalues <- lapply(cnames, function(name) {
+      paste(name, cdata[[name]], sep=" = ")
     })
-  }
-)
+    paste(allvalues, collapse = "\n")
+  })
+  
+  # A histogram
+  output$myplot <- renderPlot({
+    hist(rnorm(input$obs), main="Generated in renderPlot()")
+  })
+})
+
